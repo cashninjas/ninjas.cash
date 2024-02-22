ee64614776d95389dcb1389e9760be574aea8c0a
Mathieu Geukens <mr-zwets@protonmail.com>
2024-02-22 09:34:18 +0100

    my collection button starts a new session

--- js/collection.js
+++ js/collection.js
@@ -147,13 +147,7 @@ fullCollectionButton.onclick = () => {
 };
 myCollectionButton.onclick = async () => {
   if (session) fetchUserNinjas();
-  else if (lastSession && !urlParamAddr) {
-    const confirmReuse = confirm("The collection page is going to re-use your previous WalletConnect session, make sure you have your wallet open");
-    if (confirmReuse) {
-      session = lastSession;
-      fetchUserNinjas();
-    }
-  } else {
+  else {
     const { uri, approval } = await signClient.connect({ requiredNamespaces });
     await walletConnectModal.openModal({ uri });
     // Await session approval from the wallet.
